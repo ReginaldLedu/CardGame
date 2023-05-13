ccecec50576c5e7d5a59b58a6e82e98ac00c8d66
Regina Askerova <108342111+ReginaldLedu@users.noreply.github.com>
2023-05-13 18:44:33 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,54 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const mode =
-  process.env.NODE_ENV === "production" ? "production" : "development";
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: "./index.js",
-  mode,
-  module: {
-    rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
+    entry: "./index.js",
+    mode: isProduction ? "production" : "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "static", to: "static" }],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new MiniCssExtractPlugin(),
     ],
-  },
-  optimization: {
-    minimizer: ["...", new CssMinimizerPlugin()],
-  },
-  devtool:
-    process.env.NODE_ENV === "production" ? "hidden-source-map" : "source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()],
+    },
+
+    devtool: isProduction ? "hidden-source-map" : "source-map",
 };
