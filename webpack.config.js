12d2938ab54b4c789ae1cfbe5b1e64f90d7e9b65
Regina Askerova <108342111+ReginaldLedu@users.noreply.github.com>
2023-05-28 13:09:58 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.js$/,
                 use: "ts-loader",
                 exclude: /node_modules/,
             },
